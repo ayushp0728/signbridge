32f1d81beae18dde6e0a6184ced065a1e1c8f0ea
Priyansh Patel <youname972@gmail.com>
2024-10-27 10:29:18 -0400

    stuff

--- backend/model.py
+++ backend/model.py
@@ -18,7 +18,8 @@ def model_pipeline(file: UploadFile):
     print("2")
     try:
         # Use joblib to load the model instead of pickle
-        model = joblib.load('./Actual1_kNN_model.joblib')
+        model = joblib.load('./Actual2_model_w_angles.joblib')
+        # model = joblib.load('./Actual1_kNN_model.joblib')
         
         # Check if the model has a predict method
         if not hasattr(model, "predict"):
